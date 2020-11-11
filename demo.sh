164f758cdcfff3b3494efed5e7a08422a0b8ea24
Paulo Cesar Cuneo <cuneo.paulo.cesar@gmail.com>
2020-11-11 14:27:06 -0300

    Fix demo.sh

--- demo.sh
+++ demo.sh
@@ -10,8 +10,8 @@ register,node1,/data/,9001,2.
 register,node2,/data/,9001,3.
 EOF
 
-echoTo backups_client_1 "Agregando texto" "/data/lorem"
-echoTo backups_client_1 "Agregando otro texto" "/data/ipsum"
+echoTo backups_node1_1 "Agregando_texto" "/data/lorem"
+echoTo backups_node1_1 "Agregando_otro_texto" "/data/ipsum"
 
 
 ./nc.sh backups_default coordinator 9000 <<EOF
@@ -20,8 +20,8 @@ history,node2,/data/.
 unregister,node2,/data/.
 history,node2,/data.
 EOF
-echoTo backups_client1_1 "Agregando texto" "/data/lorem"
-echoTo backups_client1_1 "Agregando otro texto" "/data/ipsum"
+echoTo backups_node2_1 "Agregando_texto" "/data/lorem"
+echoTo backups_node2_1 "Agregando_otro_texto" "/data/ipsum"
 
 ./nc.sh backups_default coordinator 9000 <<EOF
 history,node1,/data/.
